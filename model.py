fc3d00dcf5a5ea62d17771696e5af024bfb0746a
Akshat0017 <107136241+Akshat0017@users.noreply.github.com>
2023-01-29 12:57:09 +0530

    WEEK 3 SUBMISSION
    PROJECT WEEK 3 SUBMISSION

--- model.py
+++ model.py
@@ -1,32 +1,50 @@
-import torch
+#AKSHAT SINGH
+# 210020013
+
 import numpy as np
-import torchvision
-from torch.utils.data import DataLoader, TensorDataset
-from torchvision import datasets, transforms
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
+%matplotlib inline
+import os
+import torch.optim as optim
+import random
+from sklearn.model_selection import StratifiedShuffleSplit
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from sklearn.model_selection import StratifiedShuffleSplit
-import torchvision
-from torchvision import transforms, datasets
+def set_seed(seed=42):
+    '''Sets the seed of the entire notebook so results are the same every time we run.
+    This is for REPRODUCIBILITY.'''
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    
+set_seed(42)
 
-class Net(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(28*28, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 10)
-        self.dropout = nn.Dropout(0.2)
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = x.view(-1, 28*28)
-        x = torch.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = torch.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = torch.relu(self.fc3(x))
-        x = self.dropout(x)
-        x = self.fc4(x)
+        # add dropout to the first convolutional layer
+        x = F.dropout2d(self.pool(nn.functional.relu(self.bn1(self.conv1(x)))), p=0.25)
+        x = self.pool(nn.functional.relu(self.bn2(self.conv2(x))))
+        x = x.view(-1, 16 * 5 * 5)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
-  
